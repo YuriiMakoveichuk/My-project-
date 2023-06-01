a1b8c227f3768200b15111472c4372685f1e049a
Yurii <snake77778@gmail.com>
2023-06-01 16:36:17 +0300

    add main

--- src/components/Article/ArticleList.tsx
+++ src/components/Article/ArticleList.tsx
@@ -1,6 +1,9 @@
 import { Container, Grid, Typography } from '@mui/material'
-
+import post19 from 'assets/post19.jpg'
+import post10 from 'assets/post10.jpg'
+import post20 from 'assets/post20.jpg'
 import ArticleListItem from './ArticleListItem'
+import articlesArray from 'utils/articlesArray'
 
 type Props = {}
 
@@ -15,15 +18,20 @@ const ArticleList = (props: Props) => {
                     Latest News & Blog
                 </Typography>
                 <Grid container spacing={4}>
-                    <Grid item md={4}>
-                        <ArticleListItem title="post19" />
+                    {/* <Grid item md={4}>
+                        <ArticleListItem title={post19} />
                     </Grid>
                     <Grid item md={4}>
-                        <ArticleListItem title="post10" />
+                        <ArticleListItem title={post10} />
                     </Grid>
                     <Grid item md={4}>
-                        <ArticleListItem title="post20" />
-                    </Grid>
+                        <ArticleListItem title={post20} />
+                    </Grid> */}
+                    {articlesArray.map((article) => (
+                        <Grid item md={4}>
+                            <ArticleListItem title={props.title} />
+                        </Grid>
+                    ))}
                 </Grid>
             </Container>
         </>
--- src/components/Article/ArticleListItem.tsx
+++ src/components/Article/ArticleListItem.tsx
@@ -4,9 +4,6 @@ import CardMedia from '@mui/material/CardMedia'
 import Typography from '@mui/material/Typography'
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight'
 import { Button, CardActionArea, CardActions } from '@mui/material'
-import post19 from 'assets/post19.jpg'
-import post10 from 'assets/post10.jpg'
-import post20 from 'assets/post20.jpg'
 import './ArticleListItem.scss'
 
 type Props = {
